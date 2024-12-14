4b45c69437b3722d395bd21e47d9b1a7380fe614
Pravin Ray <pravin@Pravins-MacBook-Pro.local>
2024-12-14 09:28:54 +0545

    implemented environment file to secure secrets

--- src/utils/Encrypt.js
+++ src/utils/Encrypt.js
@@ -1,6 +1,8 @@
 import CryptoJS from "crypto-js";
 
-const encryptPassword = (password, secretKey = "pravin") => {
+const encryptPassword = (password) => {
+  const secretKey = process.env.REACT_APP_SECURED_KEY;
+  console.log("REACT_APP_SECURED_KEY", process.env.REACT_APP_SECURED_KEY);
   const encryptedPassword = CryptoJS.AES.encrypt(
     password,
     secretKey
